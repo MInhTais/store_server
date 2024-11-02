ac54b539cc0f435b8fe3a353475d682be323f9f2
MInhTais <minnhtai2019cb2@gmail.com>
2024-11-02 17:40:26 +0700

    cấu trúc dự án schema, config drizzle orm

---
+++ src/common/constants/apiStatuses.enum.ts
@@ -0,0 +1,10 @@
+enum ApiStatus {
+  SUCCESS = 'success',
+  ERROR = 'error',
+  NOT_FOUND = 'not_found',
+  UNAUTHORIZED = 'unauthorized',
+  FORBIDDEN = 'forbidden',
+  VALIDATION_ERROR = 'validation_error',
+}
+
+export default ApiStatus;
